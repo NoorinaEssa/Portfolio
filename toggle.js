b34d11a96f1f790a2ff736fdc06c86896fb8cb92
Noorina <95099180+NoorinaEssa@users.noreply.github.com>
2023-08-08 11:38:08 +0500

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,17 @@
+// window.onload = function(){
+//     var body = document.getElementById("body");
+//     var button = document.getElementById("button");
+    
+//     button.onclick = function(){
+//         if(body.className == "black"){
+//             body.className = "white";
+//             //button.className = "black";
+//             body.style.backgroundColor = "red"
+//         }else{
+//             body.className = "black";
+//             button.className = "white";
+//         }
+//     };
+    
+//   };
+  
---
+++ work.js
@@ -0,0 +1,21 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_1baqi8os";
+  const templateID = "template_p6a3lzr";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+}
